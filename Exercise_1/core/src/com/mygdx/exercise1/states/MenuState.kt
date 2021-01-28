80740a53b61aa786df7f9a73b80f1de4cecf76b4
Unknown <mariubla@stud.ntnu.no>
2021-01-28 14:35:41 +0100

    added MenuState

---
+++ Exercise_1/core/src/com/mygdx/exercise1/states/MenuState.kt
@@ -0,0 +1,17 @@
+package com.mygdx.exercise1.states
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class MenuState(gsm: GameStateManager) : State(gsm) {
+    override fun update(dt: Float) {
+
+    }
+
+    override fun render(sb: SpriteBatch?) {
+
+    }
+
+    override fun handleInput() {
+
+    }
+}
