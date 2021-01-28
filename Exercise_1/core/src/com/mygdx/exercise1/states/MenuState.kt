5c7cd9e779f4aadb3705edeb0ee1ad47dce051c6
Unknown <mariubla@stud.ntnu.no>
2021-01-28 14:55:01 +0100

    added input-handling for MenuState

--- Exercise_1/core/src/com/mygdx/exercise1/states/MenuState.kt
+++ Exercise_1/core/src/com/mygdx/exercise1/states/MenuState.kt
@@ -1,17 +1,32 @@
 package com.mygdx.exercise1.states
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class MenuState(gsm: GameStateManager) : State(gsm) {
+    private var taskOneBtn : Texture = Texture("task1btn.png")
     override fun update(dt: Float) {
-
+        handleInput()
     }
 
-    override fun render(sb: SpriteBatch?) {
-
+    override fun render(sb: SpriteBatch) {
+        sb.begin()
+        sb.draw(taskOneBtn,
+                Gdx.graphics.width / 2 - (taskOneBtn.width / 2).toFloat(),
+                Gdx.graphics.height / 2.toFloat())
+        sb.end()
     }
 
     override fun handleInput() {
+        if (Gdx.input.justTouched()){
+            gsm.set(BounceState(gsm))
+            dispose()
+        }
 
+    }
+
+    override fun dispose() {
+        taskOneBtn.dispose()
     }
 }
