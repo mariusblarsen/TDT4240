bbc91046ed0adac8d71e5c862b9991c5fa8211f0
Unknown <mariubla@stud.ntnu.no>
2021-01-29 17:00:25 +0100

    update menustate to handle more states

--- Exercise_1/core/src/com/mygdx/exercise1/states/MenuState.kt
+++ Exercise_1/core/src/com/mygdx/exercise1/states/MenuState.kt
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class MenuState(gsm: GameStateManager) : State(gsm) {
     private var taskOneBtn : Texture = Texture("task1btn.png")
+    private var taskTwoBtn : Texture = Texture("task2btn.png")
+    private var taskThreeBtn : Texture = Texture("task3btn.png")
     override fun update(dt: Float) {
         handleInput()
     }
@@ -13,14 +15,28 @@ class MenuState(gsm: GameStateManager) : State(gsm) {
     override fun render(sb: SpriteBatch) {
         sb.begin()
         sb.draw(taskOneBtn,
-                Gdx.graphics.width / 2 - (taskOneBtn.width / 2).toFloat(),
+                600 - (taskOneBtn.width / 2).toFloat(),
+                Gdx.graphics.height/2.toFloat())
+        sb.draw(taskTwoBtn,
+                1200 - (taskOneBtn.width / 2).toFloat(),
+                Gdx.graphics.height / 2.toFloat())
+        sb.draw(taskThreeBtn,
+                1800 - (taskOneBtn.width / 2).toFloat(),
                 Gdx.graphics.height / 2.toFloat())
         sb.end()
     }
 
     override fun handleInput() {
+        val posX = Gdx.input.x
         if (Gdx.input.justTouched()){
-            gsm.set(BounceState(gsm))
+            when{
+                posX > 1800 -> gsm.set(AnimationState(gsm))
+                posX > 1200 -> gsm.set(MoveState(gsm))
+                else -> {
+                    gsm.set(BounceState(gsm))
+                }
+
+            }
             dispose()
         }
 
