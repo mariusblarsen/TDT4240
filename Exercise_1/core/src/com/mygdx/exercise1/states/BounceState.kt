293c3eb71e0a08bf0fd41ec2b4c69b9fa7ec7deb
Unknown <mariubla@stud.ntnu.no>
2021-01-29 14:38:33 +0100

    fixed Bouncing

--- Exercise_1/core/src/com/mygdx/exercise1/states/BounceState.kt
+++ Exercise_1/core/src/com/mygdx/exercise1/states/BounceState.kt
@@ -1,52 +1,45 @@
 package com.mygdx.exercise1.states
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Vector2
-import com.mygdx.exercise1.sprites.Helicopter
+import com.mygdx.exercise1.sprites.BounceHelicopter
 import kotlin.random.Random
 
 class BounceState(gsm : GameStateManager) : State(gsm) {
-    private var startX = Gdx.graphics.width/2.toFloat()
-    private var startY = Gdx.graphics.height/2.toFloat()
-    private var heli : Helicopter = Helicopter(50F, 100F)
+    private var startX = Gdx.graphics.width / 4.toFloat()
+    private var startY = Gdx.graphics.height / 4.toFloat()
+    private var heli : BounceHelicopter = BounceHelicopter(startX, startY)
 
     init {
         cam.setToOrtho(false,
                 Gdx.graphics.width / 2.toFloat(),
                 Gdx.graphics.height / 2.toFloat())
+        val velX = Random.nextInt(-100,100).toFloat()
+        val velY = Random.nextInt(-100, 100).toFloat()
+        heli.fly(velX, velY)
+        heli.setHeading()
     }
 
     override fun update(dt: Float) {
         handleInput()
         heli.update(dt)
-        flyRandom()
-
     }
 
     override fun render(sb: SpriteBatch) {
         sb.projectionMatrix = cam.combined
         sb.begin()
-        sb.draw(heli.getTexture(), heli.getPosition().x, heli.getPosition().y)
+        sb.draw(heli.getSprite(), heli.getPosition().x, heli.getPosition().y)
         sb.end()
     }
 
-    fun flyRandom(){
-        // Random starting velocity
-        // While true
-        // If position is border
-        // Change direction
-        val velX = Random.nextInt(-10,10).toFloat()
-        val velY = Random.nextInt(-10, 10).toFloat()
-        heli.fly(velX, velY)
-    }
-
     override fun handleInput() {
-
+        if (Gdx.input.justTouched()){
+            gsm.set(MenuState(gsm))
+            dispose()
+        }
     }
 
     override fun dispose() {
-
+        heli.dispose()
     }
 }
